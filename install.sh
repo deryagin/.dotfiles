f8f64716aa6bf677935915e9904bc2a3d219c04f
deryagin <deryagin.and@gmail.com>
2017-04-11 15:54:23 +0300

    New. install.sh added.

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+(ln -s ~/.dotfiles/.bin ~/.bin && echo '[success] .bin linked')
+(ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf && echo '[success] .tmux.conf linked')
+(ln -s ~/.dotfiles/.my.cnf ~/.my.cnf && echo '[success] .my.cnf linked')
+(ln -s ~/.dotfiles/.gitconfig ~/.gitconfig && echo '[success] .gitconfig linked')
+(ln -s ~/.dotfiles/.mongorc.js ~/.mongorc.js && echo '[success] .mongorc.js linked')
+(ln -s ~/.dotfiles/.noderc.js ~/.noderc.js && echo '[success] .noderc.js linked')
+(ln -s ~/.dotfiles/.npmrc ~/.npmrc && echo '[success] .npmrc linked')
+(ln -s ~/.dotfiles/.psqlrc ~/.psqlrc && echo '[success] .psqlrc linked')
+(ln -s ~/.dotfiles/.zlogin ~/.zlogin && echo '[success] .zlogin linked')
+(ln -s ~/.dotfiles/.zlogout ~/.zlogout && echo '[success] .zlogout linked')
+(ln -s ~/.dotfiles/.zshrc ~/.zshrc && echo '[success] .zshrc linked')
+
+#(ln -s ~/.dotfiles/.i3/config ~/.i3/config && echo '[success] i3 linked')
+#(ln -s ~/.dotfiles/.config/htop/htoprc ~/.config/htop/htoprc && echo '[success] htop linked')
+#(ln -s ~/.dotfiles/.config/mc/ini ~/.config/mc/ini && echo '[success] mc linked')
+#(ln -s ~/.dotfiles/.config/terminator/config ~/.config/terminator/config && echo '[success] terminator linked')
+
